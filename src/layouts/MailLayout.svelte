50236bd5f191fa2712985d0c87ed4816777f99dd
gsovereignty <gsovereignty@proton.me>
2024-09-24 17:32:10 +0800

    problem: scrolling doesn't work problerly

--- src/layouts/MailLayout.svelte
+++ src/layouts/MailLayout.svelte
@@ -1,16 +1,19 @@
 <script lang="ts">
 	import * as Resizable from '$lib/components/ui/resizable';
+	import { ScrollArea } from '@/components/ui/scroll-area';
 </script>
 
 <Resizable.PaneGroup direction="horizontal" class="h-full max-h-[800px] items-stretch">
 	<Resizable.Pane defaultSize={40} minSize={10} maxSize={50}
-		><slot name="list"></slot></Resizable.Pane
+		><ScrollArea class="h-screen"><slot name="list" /></ScrollArea></Resizable.Pane
 	>
 
 	<Resizable.Handle withHandle />
-	<Resizable.Pane minSize={40} maxSize={85}><slot name="problem"></slot></Resizable.Pane>
+	<Resizable.Pane minSize={40} maxSize={85}
+		><ScrollArea class="h-screen"><slot name="problem" /></ScrollArea></Resizable.Pane
+	>
 	<Resizable.Handle withHandle />
 	<Resizable.Pane minSize={10} defaultSize={10} maxSize={30}
-		><slot name="meta"></slot></Resizable.Pane
+		><ScrollArea class="h-screen"><slot name="meta" /></ScrollArea></Resizable.Pane
 	>
 </Resizable.PaneGroup>
--- src/layouts/MailList.svelte
+++ src/layouts/MailList.svelte
@@ -7,34 +7,33 @@
 	export let bloom: false; //todo
 </script>
 
-<ScrollArea class="h-screen">
-	<div class="flex flex-col gap-2 p-2 pt-0">
-		{#each items as item}
-			<button
-				on:click={() => {
-					console.log(item.event);
-					selected = item;
-				}}
-				class="flex flex-col items-start gap-2 rounded-lg border p-3 text-left text-sm transition-all hover:bg-accent"
-			>
-				<div class="flex w-full flex-col gap-1">
-					<div class="flex items-center">
-						<div class="flex items-center gap-2">
-							<div class="font-semibold">{item.tldr}</div>
-							{#if !bloom}
-								<span class="flex h-2 w-2 rounded-full bg-blue-600" />
-							{/if}
-						</div>
-						<div class="ml-auto text-xs text-foreground">
-							{formatTimeAgo(new Date(item.event.created_at * 1000))}
-						</div>
+<div class="flex flex-col gap-2 p-2 pt-0">
+	{#each items as item}
+		<button
+			on:click={() => {
+				console.log(item.event);
+				selected = item;
+			}}
+			class="flex flex-col items-start gap-2 rounded-lg border p-3 text-left text-sm transition-all hover:bg-accent"
+		>
+			<div class="flex w-full flex-col gap-1">
+				<div class="flex items-center">
+					<div class="flex items-center gap-2">
+						<div class="font-semibold">{item.tldr}</div>
+						{#if !bloom}
+							<span class="flex h-2 w-2 rounded-full bg-blue-600" />
+						{/if}
 					</div>
-					<div class="text-xs font-medium">{item.para}</div>
+					<div class="ml-auto text-xs text-foreground">
+						{formatTimeAgo(new Date(item.event.created_at * 1000))}
+					</div>
 				</div>
-				<div class="line-clamp-2 text-xs text-muted-foreground">
-					{'todo: add full page description of problem'.substring(0, 300)}
-				</div>
-				<!-- {#if item.labels.length}
+				<div class="text-xs font-medium">{item.para}</div>
+			</div>
+			<div class="line-clamp-2 text-xs text-muted-foreground">
+				{'todo: add full page description of problem'.substring(0, 300)}
+			</div>
+			<!-- {#if item.labels.length}
 					<div class="flex items-center gap-2">
 						{#each item.labels as label}
 							<Badge variant={get_badge_variant_from_label(label)}>
@@ -43,7 +42,6 @@
 						{/each}
 					</div>
 				{/if} -->
-			</button>
-		{/each}
-	</div>
-</ScrollArea>
+		</button>
+	{/each}
+</div>
